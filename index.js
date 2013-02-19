d22be27f7a1fdde60056ec39b52628721e364217
Justin Walgran <jwalgran@azavea.com>
2013-02-19 13:54:13 -0500

    Refactor middleware pattern matching
    I replaced the deeply nested code and if/else if/else blocks with an
    array of handler functions and a processing loop.

--- index.js
+++ index.js
@@ -2,32 +2,30 @@ var url = require('url');
 var shade = require('shade');
 var pkg = require('./package.json');
 
+var keyNotFound = function (res) {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end("key not found");
+};
+
+var badRequest = function (res, msg) {
+    res.statusCode = 400;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end(msg);
+};
+
 exports = module.exports = function (opts) {
-    var middleware = function (req, res, next) {
-        opts = opts || {};
-        shade(opts.datastore || "./datastore", function(err, db) {
-            var parsedUrl = url.parse(req.url, true);
-            var rootMatch = parsedUrl.pathname.match(/^\/$/);
-            var goMatch = parsedUrl.pathname.match(/^\/([a-f0-9]{8})$/);
-            var itemMatch = parsedUrl.pathname.match(/^\/hashes\/([a-f0-9]{8})$/);
-            if (err) {
-                res.statusCode = code;
-                res.setHeader('Content-Type', 'text/plain');
-                res.end(msg);
-            } else if (rootMatch && req.method === "GET") {
+    opts = opts || {};
+
+    var handlers = [
+        {
+            pattern: /^\/$/,
+            GET: function getRoot (db, match, req, res, next) {
                 res.setHeader('Content-Type', 'text/plain');
                 res.end(pkg.name + ' ' + pkg.version);
-            } else if (itemMatch && req.method === "GET") {
-                db.load(itemMatch[1], function (err, data) {
-                    if (!err) {
-                        res.setHeader('Content-Type', 'text/plain');
-                        res.end(data);
-                    } else {
-                        next();
-                    }
-                });
-            } else if (rootMatch && req.method === "POST") {
-                if (req.body && req.body.data) {
+            },
+            POST: function postRoot (db, match, req, res, next) {
+                 if (req.body && req.body.data) {
                     res.setHeader('Content-Type', 'text/plain');
                     db.save(req.body.data, function (err, key) {
                         if (!err) {
@@ -38,24 +36,62 @@ exports = module.exports = function (opts) {
                         }
                     });
                 } else {
-                    res.statusCode = 400;
-                    res.setHeader('Content-Type', 'text/plain');
-                    res.end("POST body must have a 'data' member");
+                    badRequest(res, 'POST body must have a "data" member');
                 }
-            } else if (goMatch && req.method === "GET") {
-                db.load(goMatch[1], function (err, data) {
+            }
+        },
+        {
+            pattern: /^\/([a-f0-9]{8})$/,
+            GET: function getData (db, match, req, res, next) {
+                db.load(match[1], function (err, data) {
                     var redirectUrl = opts.redirectUrl || "default-redirect-url";
                     if (!err) {
                         res.writeHead(302, {'Location': redirectUrl + "#" + data});
                         res.end();
                     } else {
-                        next();
+                        keyNotFound(res);
                     }
                 });
+            }
+        },
+        {
+            pattern: /^\/hashes\/([a-f0-9]{8})$/,
+            GET: function getRedirect (db, match, req, res, next) {
+                db.load(match[1], function (err, data) {
+                    if (!err) {
+                        res.setHeader('Content-Type', 'text/plain');
+                        res.end(data);
+                    } else {
+                        keyNotFound(res);
+                    }
+                });
+            }
+        }
+    ];
+
+    var middleware = function (req, res, next) {
+        shade(opts.datastore || "./datastore", function(err, db) {
+            var i, handled, match;
+            var pathname = url.parse(req.url).pathname;
+            if (!err) {
+                for (i = 0; i < handlers.length; i++) {
+                    match = pathname.match(handlers[i].pattern);
+                    if (match && handlers[i][req.method]) {
+                        handlers[i][req.method](db, match, req, res, next);
+                        handled = true;
+                        break;
+                    }
+                }
+                if (!handled) {
+                    next();
+                }
             } else {
-                next();
+                res.statusCode = 500;
+                res.setHeader('Content-Type', 'text/plain');
+                res.end("data store failure");
             }
         });
     };
+
     return middleware;
 };
